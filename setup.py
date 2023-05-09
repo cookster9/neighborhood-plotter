4739b4466dcd13c920ba3d877ef3dce5a379ca3e
Andrew Cook <andrewcook@Andrews-Air.attlocal.net>
2023-05-09 17:55:35 -0500

    Made setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='plot_addresses',
+    version='0.5',
+    packages=['plot_addresses'],
+    url='https://github.com/cookster9/neighborhood-plotter',
+    license='MIT',
+    author='andrewcook',
+    author_email='acook999@gmail.com',
+    description='Create a folium map of nashville home sales (data not included)'
+)
